41af2d340b4da85d21d6c77efc510fbff0a22e3e
Hazem AbuMostafa <hbluebrince@gmail.com>
2015-04-26 05:26:37 +0200

    The new DataSetReader
    Automating the whole data set-related process by using
    getNextTrainSample and getNextTestSample while not returning null.

---
+++ NeuralNetworksFiles/DataSets/DataSetReader.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace NeuralNetworks
+{
+	public class DataSetReader
+	{
+		private string globalPath;
+		
+		public int currentTrainSample; //One-based.
+		public int currentTrainClass;  //Zero-based.
+		public int currentTestSample; //One-based.
+		public int currentTestClass;  //Zero-based.
+
+		private string sampleNameFormat;
+		public readonly string[] classLabels;
+
+		public readonly DataSetType type;
+		public readonly int classes;	//Number of classes in the data set.
+		public readonly int trainSamplesPerClass;	//Number of train samples per class.
+		public readonly int testSamplesPerClass;	//Number of test samples per class.
+		public readonly int featuresPerSample;	//Number of features per sample.
+
+		//Normalization data:
+		public readonly bool normalized;
+		private double[] mean;
+		private double[] max;
+
+		public DataSetReader(string path, DataSetType type, bool normalizeData)
+		{
+			this.globalPath = path;
+			this.type = type;
+			this.normalized = normalizeData;
+
+			this.currentTrainSample = 0;
+			this.currentTrainClass = 0;
+
+			this.currentTestSample = 0;
+			this.currentTestClass = 0;
+
+			switch(type){
+			case DataSetType.HeadOrientation:
+				this.classes = 3;
+				this.featuresPerSample = 2500;
+				this.trainSamplesPerClass = 50;
+				this.testSamplesPerClass = 30;
+				this.sampleNameFormat = "head ({0}).png";
+				this.classLabels = new string[] {"Front", "Left", "Right"};
+				break;
+			}
+
+			if(normalized){
+				readDataFromFile("mean");
+				readDataFromFile("max");
+			}
+		}
+
+		public double[] getNextTrainSample()
+		{
+			string trainPath = globalPath + "Training/";
+			double[] sampleData = new double[featuresPerSample]; //Initialize data container.
+
+			string fullPath;
+			if(nextTrainIndex()){
+				fullPath = trainPath + this.classLabels[currentTrainClass]
+					+ "/" + string.Format(this.sampleNameFormat, currentTrainSample);
+				sampleData = readImage(fullPath);
+			}
+			else sampleData = null;
+
+			return sampleData;
+		}
+
+		public double[] getNextTestSample()
+		{
+			string testPath = globalPath + "Testing/";
+			double[] sampleData = new double[featuresPerSample]; //Initialize data container.
+
+			string fullPath;
+			if(nextTestIndex()){
+				fullPath = testPath + this.classLabels[currentTestClass]
+					+ "/" + string.Format(this.sampleNameFormat, currentTestSample);
+				sampleData = readImage(fullPath);
+			}
+			else sampleData = null;
+
+			return sampleData;
+		}
+
+		private bool nextTrainIndex()
+		{
+			if(this.currentTrainSample > this.trainSamplesPerClass
+				&& this.currentTrainClass >= this.classes)
+				return false;
+
+			this.currentTrainSample++;
+			if(this.currentTrainSample > this.trainSamplesPerClass){
+				this.currentTrainClass++;
+				this.currentTrainSample = 1;
+				if(this.currentTrainClass >= this.classes){
+					resetTrainIndex();
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		private bool nextTestIndex()
+		{
+			if(this.currentTestSample > this.testSamplesPerClass
+				&& this.currentTestClass >= this.classes)
+				return false;
+
+			this.currentTestSample++;
+			if(this.currentTestSample > this.testSamplesPerClass){
+				this.currentTestClass++;
+				this.currentTestSample = 1;
+				if(this.currentTestClass >= this.classes){
+					resetTestIndex();
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		public double[] readImage(string path)
+		{
+			Bitmap bitmap = BitmapTools.toGrayScale(new Bitmap(path));
+			double[] data = BitmapTools.to1D(bitmap);
+
+			//Normalize?
+			if(this.normalized)
+				data = norm(data);
+
+			return data;
+		}
+
+		private void readDataFromFile(string type)
+		{
+			string[] temp;
+			if(type == "mean"){
+				string mean = File.ReadAllText(globalPath + "Training/" + "mean.txt");
+				temp = mean.Split(',');
+				this.mean = Array.ConvertAll(temp, Convert.ToDouble);
+			}
+			else if(type == "max"){
+				string max = File.ReadAllText(globalPath + "Training/" + "max.txt");
+				temp = max.Split(',');
+				this.max = Array.ConvertAll(temp, Convert.ToDouble);
+			}
+		}
+
+		public void resetTrainIndex()
+		{
+			this.currentTrainSample = 0;
+			this.currentTrainClass = 0;
+		}
+
+		public void resetTestIndex()
+		{
+			this.currentTestSample = 0;
+			this.currentTestClass = 0;
+		}
+
+		/// <summary>Normalize a single value using the data set normalization parameters.</summary>
+		/// <returns>The normalized value.</returns>
+		public double[] norm(double[] value)
+		{
+			double[] result = new double[value.Length];
+			for(int i=0; i<result.Length; i++)
+				result[i] = (value[i] - this.mean[i]) / this.max[i];
+
+			return result;
+		}
+	}
+}
---
+++ NeuralNetworksFiles/GeneralHelpers/BitmapTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+
+namespace NeuralNetworks
+{
+	static class BitmapTools
+	{
+		public static Bitmap toGrayScale(Bitmap bitmap)
+		{
+            Bitmap newBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+			Color oldColor, newColor;
+			int greyValue;
+            for(int j=0; j < newBitmap.Height; j++)
+                for(int i=0; i < newBitmap.Width; i++){
+                    oldColor = bitmap.GetPixel(i, j);
+                    greyValue = (int)(0.2126*oldColor.R + 0.7152*oldColor.G + 0.0722*oldColor.B);
+                    newColor = Color.FromArgb(greyValue, greyValue, greyValue);
+                    newBitmap.SetPixel(i, j, newColor);
+                }
+            
+			return newBitmap;
+        }
+
+		public static double[] to1D(Bitmap bitmap)
+		{
+			double[] data = new double[bitmap.Width * bitmap.Height];
+
+			for(int i=0; i < bitmap.Width; i++)
+                for(int j=0; j < bitmap.Height; j++)
+					data[(bitmap.Width * j) + i] = bitmap.GetPixel(i, j).R;
+
+			return data;
+		}
+	}
+}
